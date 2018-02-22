f80189cce4d19a61bf84afe7b175e20ef4ff4ec7
Jaap Donker <jaapdonker@gmail.com>
2018-02-22 13:23:57 +0100

    Adds header

--- backend.js
+++ backend.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json())
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, DELETE")
     next()
 })
 app.use(function(req, res, next) {
@@ -42,7 +43,7 @@ app.get('/game/:id/', (req, res) => {
 
 app.put('/game/:id/', (req, res) => {
     if (!games[req.params.id]) return res.sendStatus(404)
-    games[req.params.id] = req.body
+    games[req.params.id].board = req.body
     res.sendStatus(200)
 })
 
