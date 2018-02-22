dc2a9803106cd5e6744fec3c52b358726581c99e
Tino <tino@solarmonkey.nl>
2018-02-22 12:31:47 +0100

    Flatten board

--- backend.js
+++ backend.js
@@ -29,7 +29,7 @@ app.post('/new-game/', (req, res) => {
     const id = Math.random().toString(36).substr(2, 9)
     games[id] = {
         id: id,
-        board: newBoard()
+        board: flatten(newBoard())
     }
     res.status(201).send(games[id])
 })
@@ -85,4 +85,54 @@ function newBoard() {
     initSquares[4][4] = 'X'
     initSquares[4][3] = 'O'
     return initSquares
+}
+
+function flatten(board) {
+    return board.reduce((results, row) => {
+        return results.concat(row)
+    }, [])
+}
+
+function flipSquares(board, position, xIsNext) {
+    let modifiedBoard = null;
+    // Calculate row and col of the starting position
+    let [startX, startY] = [position % 8, (position - position % 8) / 8];
+
+    if (board[position] !== null) {
+        return null;
+    }
+
+    // Iterate all directions, these numbers are the offsets in the array to reach next sqaure
+    [1, 7, 8, 9, -1, -7, -8, -9].forEach((offset) => {
+        let flippedSquares = modifiedBoard ? modifiedBoard.slice() : board.slice();
+        let atLeastOneMarkIsFlipped = false;
+        let [lastXpos, lastYPos] = [startX, startY];
+
+        for (let y = position + offset; y < 64; y = y + offset) {
+
+            // Calculate the row and col of the current square
+            let [xPos, yPos] = [y % 8, (y - y % 8) / 8];
+
+            // Fix when board is breaking into a new row or col
+            if (Math.abs(lastXpos - xPos) > 1 || Math.abs(lastYPos - yPos) > 1) {
+                break;
+            }
+
+            // Next square was occupied with the opposite color
+            if (flippedSquares[y] === (!xIsNext ? 'X' : 'O')) {
+                flippedSquares[y] = xIsNext ? 'X' : 'O';
+                atLeastOneMarkIsFlipped = true;
+                [lastXpos, lastYPos] = [xPos, yPos];
+                continue;
+            }
+            // Next aquare was occupied with the same color
+            else if ((flippedSquares[y] === (xIsNext ? 'X' : 'O')) && atLeastOneMarkIsFlipped) {
+                flippedSquares[position] = xIsNext ? 'X' : 'O';
+                modifiedBoard = flippedboard.slice();
+            }
+            break;
+        }
+    });
+
+    return modifiedBoard;
 }
