ab3c3704461f223a952d632c2778cf0c9fda873d
Tino <tino@solarmonkey.nl>
2018-02-23 13:35:31 +0100

    Clean up index.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,4 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import Game from './game.js';
 
-const socket = new WebSocket('ws://localhost:4000/');
-
 ReactDOM.render(<Game />, document.getElementById('root'));
-
-socket.addEventListener('open', function (event) {
-    socket.send('Hello Server!');
-});
-
-// Listen for messages
-socket.addEventListener('message', function (event) {
-    console.log('Message from server ', event.data);
-});
-window.socket = socket
